da6f077b76ff35d655922dde8ca2c0d16fbdda60
Tamas Molnar <molnart90@gmail.com>
2023-12-21 22:06:25 +0100

    add eslint + first custom component

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Main/>;
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +13,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+  
+  
